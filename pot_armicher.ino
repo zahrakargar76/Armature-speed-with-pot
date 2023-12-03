9e982e34f690d22e8eca019d835454d121f92098
zahrakargar76 <148703264+zahrakargar76@users.noreply.github.com>
2023-12-03 07:42:20 -0800

    Add files via upload

---
+++ pot_armicher.ino
@@ -0,0 +1,25 @@
+int enA = 10; 
+int in1 = 9; 
+int in2 = 8;
+int pot = A0;
+
+
+void setup()
+{
+
+  pinMode(enA, OUTPUT);
+  pinMode(in1, OUTPUT); 
+  pinMode(in2, OUTPUT);
+  pinMode(pot, INPUT);  
+
+}
+
+void loop()
+{
+  int potValue = analogRead(pot);
+  int motorSpeed = map(potValue, 0, 1023, 0, 255);
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, LOW);
+  analogWrite(enA, motorSpeed);
+
+}
